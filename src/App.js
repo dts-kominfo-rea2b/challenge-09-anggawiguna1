e7f19462fffcd2f76288d8277ec3565405ac887e
anggawiguna1 <angga065109205@gmail.com>
2022-06-19 21:56:00 +0700

    Challenge done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <div className='Container'>
+        <Header/>
+        {contacts.map((data,index) => {
+          return <Contact key={index} data={data}/>
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import "./Contact.css";
 
-const Contact = () => {
+const Contact = ({ data }) => {
     return (
-        <></>
+        <div className="Contact">
+            <div className="Img">
+                <img src={data.photo} alt={data.name} />
+            </div>
+            <div className="Detail">
+                <h2>{data.name}</h2>
+                <p>{data.phone}</p>
+                <p>{data.email}</p>
+            </div>
+        </div>
     )
 }
 
